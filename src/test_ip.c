361d969ee3ee61e9a3e286e3ace8929c8ac779bf
qasem <qasemt@gmail.com>
2017-04-26 17:05:25 +0430

    fix error

--- src/test_ip.c
+++ src/test_ip.c
@@ -5,19 +5,29 @@
 
 // need link with Ws2_32.lib
 #pragma comment(lib, "Ws2_32.lib")
+#define ipToInt(x) inet_addr(x)
+
 
 void run_test_ip(void)
 {
 	uint64_t ip,netip,netmask,netstart,netend;
 	//unsigned long ulAddr = INADDR_NONE;
+	char* ipStr;
+		struct in_addr paddr;
 
- 
+	ip = ipToInt("50.22.100.250"); // value to check
+	//-------- to string -------
 
-	 ip = inet_addr("50.22.100.250"); // value to check
-	 netip = inet_addr("50.22.1.1");; // network ip to compare with
-	 netmask = inet_addr("255.255.0.0"); // network ip subnet mask
-	 netstart = (netip & netmask); // first ip in subnet
-	 netend = (netstart | ~netmask); // last ip in subnet
+	paddr.S_un.S_addr = ip;
+
+	ipStr = inet_ntoa(paddr);
+
+	
+	//-=----------
+	netip = ipToInt("50.22.1.1");; // network ip to compare with
+	netmask = ipToInt("255.255.0.0"); // network ip subnet mask
+	netstart = (netip & netmask); // first ip in subnet
+	netend = (netstart | ~netmask); // last ip in subnet
 	if ((ip >= netstart) && (ip <= netend))
 	{
 		// is in subnet range...
