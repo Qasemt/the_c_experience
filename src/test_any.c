f80d13b6c772a332cb09aa31675eab124f6def97
qasem <taaheri@outlook.com>
2017-08-14 19:59:46 +0430

    ff

--- src/test_any.c
+++ src/test_any.c
@@ -3,20 +3,23 @@
 
 void run_test_any()
 {
-	
-		int tmp=0;
-	int a1 = 0x1 ;
-	int a2 = 0x3 ;
-	int a3 =0x4  ;
-	int a4 =0x8  ;
-	int base = a1|a2;
 
-   if((base & a3)==a3)
-   {
-	  int i=0;
-
-   }
+	int tmp = 0;
+	int a0 = 1 << 0;
 	
+	int a1 = 2 << 1;
+	int a2 = 1 << 2;
+	int a3 = 1 << 3;
+	int a4 = 1 << 4;
+	int a7 = 1 << 9;
+	int base = a1 | a0;
 
-	
+	if ((base & a1) == a1)
+	{
+		int i = 0;
+
+	}
+
+
+
 }
