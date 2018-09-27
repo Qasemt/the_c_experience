62ead2b637a0368eb88a697700de8b502295e946
qasem <qasemt@gmail.com>
2018-09-27 12:39:33 +0330

    dd

--- src/main.c
+++ src/main.c
@@ -1,8 +1,7 @@
 // testc.cpp : Defines the entry point for the console application.
 //
-#include <stdlib.h>
-#include <tchar.h>
-#include <conio.h>
+
+#include <stdlib.h> 
 #include "string.h"
 #include "common.h"
 #include "test_hash_table.h"
@@ -15,18 +14,13 @@
 #include "test_convert_to.h"
 //#include <winbase.h>
 #include  "test_time.h"
-#include "test_elapse.h"
 
-
-int _tmain(int argc, _TCHAR* argv[])
+int main(int argc, char* argv[])
 {
-    /*INT64 f1=_I64_MAX;
+/*INT64 f1=_I64_MAX;
 	UINT64 f2=_I64_MAX;
 	InterlockedIncrement64(&f1);
 	f2=f1;*/
-
-	//run_elapse_();
-
 	run_test_Time();
 	//__________________________________________
 	//	run_convert_to();
@@ -34,7 +28,7 @@ int _tmain(int argc, _TCHAR* argv[])
 	//__________________________________________
 	//run_test_any();
 	//__________________________________________
-		//run_test_curl();
+	//	run_test_curl();
 	//__________________________________________
 	//	run_test_ip();
 	//__________________________________________
--- src/test_time.c
+++ src/test_time.c
@@ -1,102 +1,121 @@
-
+﻿
 
 #include <stdio.h>
 
 #include <windows.h>
 
 #include "test_time.h"
+#include <time.h>
 
 
-INT64 addTime2(INT64 current_time,INT64 value,INT64 typeTime)
+INT64 addTime2(INT64 current_time, INT64 value, INT64 typeTime)
 {
-	if(value<0)
-	{
-		current_time -= (_abs64(value)*typeTime);
-		current_time-=(current_time%typeTime);
-	}
-	else
-	{
-		current_time += (_abs64(value)*typeTime);
-		current_time+=(current_time%typeTime);
-	}
-	return current_time;
+    if (value < 0)
+    {
+        current_time -= (_abs64(value)*typeTime);
+        current_time -= (current_time%typeTime);
+    }
+    else
+    {
+        current_time += (_abs64(value)*typeTime);
+        current_time += (current_time%typeTime);
+    }
+    return current_time;
 
 
 }
 
 INT64 addTime3(time_t current_time, int value, TypeTime typeTime)
 {
-	struct tm t;
+    struct tm t;
 
-	t= *localtime(&current_time);
-	if (typeTime==TypeTime_Hour)
-		t.tm_hour += value;
-	if (typeTime == TypeTime_Minute) t.tm_min += value;
-	else t.tm_sec += value;
+    t = *localtime(&current_time);
+    if (typeTime == TypeTime_Hour)
+        t.tm_hour += value;
+    if (typeTime == TypeTime_Minute) t.tm_min += value;
+    else t.tm_sec += value;
 
-	current_time = mktime(&t);
-	return current_time;
+    current_time = mktime(&t);
+    return current_time;
 }
 
-INT64 setOnlyTime(time_t raw_time,int h,int m,int s)
+INT64 setOnlyTime(time_t raw_time, int h, int m, int s)
 {
-	time_t raw_time_result;
-	struct  tm loc;
-	loc= *localtime(&raw_time);
+    time_t raw_time_result;
+    struct  tm loc;
+    loc = *localtime(&raw_time);
 
-	loc.tm_year=100; //year 2000 - 1900 = 100
-	loc.tm_mon=1;
-	loc.tm_mday=1;
-	loc.tm_hour =h;
-	loc.tm_min=m;
-	loc.tm_sec=s;
-	loc.tm_isdst=FALSE;
-	raw_time_result =	mktime(&loc);
-	return raw_time_result;
+    loc.tm_year = 100; //year 2000 - 1900 = 100
+    loc.tm_mon = 1;
+    loc.tm_mday = 1;
+    loc.tm_hour = h;
+    loc.tm_min = m;
+    loc.tm_sec = s;
+    loc.tm_isdst = FALSE;
+    raw_time_result = mktime(&loc);
+    return raw_time_result;
 }
 char buffer[26];
 const char* timeString(time_t raw_time)
 {
-	struct tm * timeinfo;
-	buffer[0]='\0';
-	timeinfo = localtime(&raw_time);
-	
-	strftime(buffer, _countof(buffer), "%Y-%m-%d %H:%M:%S", timeinfo);
-	return  buffer;
+    struct tm * timeinfo;
+    buffer[0] = '\0';
+    timeinfo = localtime(&raw_time);
+
+    strftime(buffer, _countof(buffer), "%Y-%m-%d %H:%M:%S", timeinfo);
+    return  buffer;
 }
 
-INT64 diffTime(INT64 time1,INT64 time2,INT64 typeTime)
+INT64 diffTime(INT64 time1, INT64 time2, INT64 typeTime)
 {
-	return  (time1-time2)/typeTime;
+    return  (time1 - time2) / typeTime;
 
 }
 
+
+
+void get_time_zone_posix()
+{
+    TIME_ZONE_INFORMATION tz;    DWORD dwRet = GetTimeZoneInformation(&tz);    int tz_offset = (tz.Bias * 60);    if (dwRet == TIME_ZONE_ID_DAYLIGHT)        tz_offset += (tz.DaylightBias * 60);
+    printf("%d\n", tz_offset);
+    //--------------
+    time_t rawtime = time(NULL);
+    struct tm *ptm = gmtime(&rawtime);
+    // Request that mktime() looksup dst in timezone database
+    ptm->tm_isdst = -1;
+    time_t gmt = mktime(ptm);
+    int offset = -(int)difftime(rawtime, gmt);
+    printf("%d\n", offset);
+}
 
 
 void run_test_Time(void)
 {
 
-	INT64 start_time;
-	INT64 end_time;
-	INT64 secs_diff;
-	INT64 hours_diff;
-	INT64 mint;
-	//C++
-	/*SYSTEMTIME s_time;
-	SYSTEMTIME stNow;
-	FILETIME ftNow;
-	FILETIME ftNow1;
-	GetSystemTime(&stNow);
-	INT64 res=0;
-	SystemTimeToFileTime(&stNow,((FILETIME*)&res));
-	char time[200]={0};*/
-	//-----------------------------------------------
-	GetSystemTimeAsFileTime((FILETIME*)&start_time);
+    /*INT64 start_time;
+    INT64 end_time;
+    INT64 secs_diff;
+    INT64 hours_diff;
+    INT64 mint;*/
+    //C++
+    /*SYSTEMTIME s_time;
+    SYSTEMTIME stNow;
+    FILETIME ftNow;
+    FILETIME ftNow1;
+    GetSystemTime(&stNow);
+    INT64 res=0;
+    SystemTimeToFileTime(&stNow,((FILETIME*)&res));
+    char time[200]={0};*/
+    //-----------------------------------------------
+    //GetSystemTimeAsFileTime((FILETIME*)&start_time);
 
-	GetSystemTimeAsFileTime((FILETIME*)& end_time);
-	//---------------------
-	secs_diff = (end_time-start_time)/_SECOND;
-	//---------------------
-	mint =addTime2(end_time,-27,_HOUR);
-	hours_diff=diffTime(start_time,mint,_DAY);
+    //GetSystemTimeAsFileTime((FILETIME*)& end_time);
+    //---------------------
+    //secs_diff = (end_time-start_time)/_SECOND;
+    //---------------------
+    //mint =addTime2(end_time,-27,_HOUR);
+    //hours_diff=diffTime(start_time,mint,_DAY);
+
+    //===============================
+    get_time_zone_posix();
 }
